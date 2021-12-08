809bd621038ecca71268a328f17a2217a9931e65
JJongS <kimjs3061@naver.ocm>
2021-12-08 23:33:53 +0900

    Spring Boot, thymeleaf를 이용해서 form 전송 방법 알아보기 - 2

--- src/main/java/com/godcoder/myhome/model/Board.java
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -13,7 +13,7 @@ public class Board {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String title;
     private String content;
 
