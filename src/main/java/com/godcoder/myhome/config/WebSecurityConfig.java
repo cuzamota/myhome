dd907b161986cddcc3c2a7001dabe3c987e4447a
ESTSW-EQPMNT-NB-005 <kimjs3061@naver.com>
2021-12-26 23:04:51 +0900

    사용자 테이블을 만들고 Spring Security를 적용하여 인증 및 권한 처리 로그인, 회원가입, 로그아웃, 페이지 권한 처리(완료)

--- src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
+++ src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.godcoder.myhome.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,54 +12,44 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/")
-                    .permitAll()
-                    .anyRequest().authenticated()
-                    .and()
+                .antMatchers("/", "/account/register", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
+                .loginPage("/account/login")
+                .permitAll()
+                .and()
                 .logout()
-                    .permitAll();
+                .permitAll();
     }
-
-    @Autowired
-    private DataSource dataSource;
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth)
             throws Exception {
         auth.jdbcAuthentication()
-        //Authentication 로그인, 인증 처리
                 .dataSource(dataSource)
-                .passwordEncoder(passwordEncoder())
                 .usersByUsernameQuery("select username, password, enabled "
-                        + "from users "
+                        + "from user "
                         + "where username = ?")
-                .authoritiesByUsernameQuery("select username, name "
-                //Authorization 권한 처리
+                .authoritiesByUsernameQuery("select u.username, r.name "
                         + "from user_role ur inner join user u on ur.user_id = u.id "
                         + "inner join role r on ur.role_id = r.id "
-                        + "where username = ?");
+                        + "where u.username = ?");
     }
-    //@OneToOne     ex) user  -  user_detail
-    //@OneToMany    ex) user  -  board
-    //@ManyToOne    ex) board -  user
-    //@ManyToMany   ex) user  -  role
 
-    //@Bean
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
 }
